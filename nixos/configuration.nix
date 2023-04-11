4028d133c74d02b20ed8e5309aba97e5f9edd9ed
Ryan S <ryansouza@usp.br>
2023-04-11 11:11:57 -0300

    feat: flake reference

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -2,7 +2,7 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ config, pkgs, ... }:
+{ config, pkgs, inputs, lib, ... }:
 
 {
   imports =
@@ -87,6 +87,10 @@
 
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
+  
+  # Enable a way to reference flake attributes in nixpkgs
+  nix.registry = lib.mapAttrs (_: value: { flake=value; }) inputs;
+
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.ryrden = {
