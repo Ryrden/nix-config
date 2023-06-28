0014301fb19e10b5b56022ac632649566e8b08b4
Ryan S <ryansouza@usp.br>
2023-06-28 15:37:52 -0300

    bootloader refactor

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -11,9 +11,14 @@
     ];
 
   # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.efi.efiSysMountPoint = "/boot/efi";
+  boot.loader = {
+    systemd-boot.enable = true;
+    efi = {
+      canTouchEfiVariables = true;
+      efiSysMountPoint = "/boot/efi";
+    };
+    timeout = 10; # Wait 10 seconds before booting the default entry.
+  };
 
   networking.hostName = "nixos-ryans"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
